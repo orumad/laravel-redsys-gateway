5d15fad7e3eb530ca3917527afaba69017deadef
Daniel Muñoz <dev@danielmunoz.io>
2020-07-07 11:40:54 +0200

    Added pest as test framework

--- src/Controllers/RedsysNotificationController.php
+++ src/Controllers/RedsysNotificationController.php
@@ -14,18 +14,18 @@ class RedsysNotificationController
         if ($parameters = request()->input('Ds_MerchantParameters')) {
             $redsysNotification->setUp(request()->input('Ds_MerchantParameters'));
 
-        // The signature must be valid
-        if ($redsysNotification->isValidSignature(request()->input('Ds_Signature'))) {
-            $redsysPayment =
-                RedsysPayment::where('ds_merchant_order', $redsysNotification->order)
-                    ->firstOrFail();
+            // The signature must be valid
+            if ($redsysNotification->isValidSignature(request()->input('Ds_Signature'))) {
+                $redsysPayment =
+                    RedsysPayment::where('ds_merchant_order', $redsysNotification->order)
+                        ->firstOrFail();
 
-                // Add notification to the payment (DB)
-                $redsysPayment->redsysNotifications()->save($redsysNotification);
+                    // Add notification to the payment (DB)
+                    $redsysPayment->redsysNotifications()->save($redsysNotification);
 
-            // Emit event to notify the notification to the app
-            event(new RedsysNotificationArrived($redsysNotification));
-        }
+                // Emit event to notify the notification to the app
+                event(new RedsysNotificationArrived($redsysNotification));
+            }
 
             // Signature is invalid: do nothing
         }
